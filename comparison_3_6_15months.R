c87200340d4b3c7d1dc92bc4dcecb9199110d6c2
laupierre <laupierre@users.noreply.github.com>
2023-05-04 15:14:27 +0200

    Update and rename comparison_3_vs_15months.R to comparison_3_6_15months.R

---
+++ comparison_3_6_15months.R
@@ -0,0 +1,23 @@
+library (openxlsx)
+
+m3 <- read.xlsx ("GLONG_vs_OLA_2023_selected_mice_3_months.xlsx")
+colnames (m3) <- paste (colnames (m3), "-3months-2022", sep="")
+
+m6 <- read.xlsx ("GLONG_vs_OLA_2023_selected_mice_6_months.xlsx")
+colnames (m6) <- paste (colnames (m6), "-6months-2022", sep="")
+
+m15 <- read.xlsx ("GLONG_vs_OLA_2023_selected_mice_15_months.xlsx")
+colnames (m15) <- paste (colnames (m15), "-15months-2022", sep="")
+
+mco <- merge (m3, m6, by.x="Geneid-3months-2022", by.y="Geneid-6months-2022")
+mco <- merge (mco, m15, by.x="Geneid-3months-2022", by.y="Geneid-15months-2022")
+
+mco <- mco[ ,grep ("type|external|description|lfcSE|stat|pvalue|mgi|GLONG|OLA" , colnames (mco), invert=TRUE)]
+mco <- mco [order (mco$`padj-3months-2022`), ]
+colnames (mco)[colnames (mco) == "Geneid-3months-2022"] <- "Geneid"
+mco$stats <- ifelse (mco$`padj-3months-2022` < 0.05 & mco$`padj-15months-2022` < 0.05 & mco$`padj-6months-2022` < 0.05, "Yes","No")
+mco$trend <- ifelse (mco$`log2FoldChange-3months-2022` < 0 & mco$`log2FoldChange-15months-2022` < 0 & mco$`log2FoldChange-6months-2022` < 0, "Down","No")
+mco$trend [mco$`log2FoldChange-3months-2022` > 0 & mco$`log2FoldChange-15months-2022` > 0 & mco$`log2FoldChange-months-2022` > 0] <- "Up"
+write.xlsx (mco, "Comparison of GLONG vs OLA at 3, 6 and 15 months.xlsx", rowNames=F)
+
+
